4ea62692f68ccbabb68a37870603de45c664b254
ElainaSauder <161645329+ElainaSauder@users.noreply.github.com>
2024-03-05 17:47:33 -0600

    changed code

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,29 +1,32 @@
 // Interactive Scene-Knowing Your Bones
 // Elaina Sauder
-// Feburary th, 2024
+// Feburary 11th, 2024
 //
 // Extra for Experts:
 // - Taking in User Input to answer the questions I ask
 
-// the plan is to create a guessing game that helps ou learn the bones in the bod, by letting you assemble them. slash having you click on the correct bone.
+// the plan is to create a guessing game that helps you learn the bones in the body, by letting you . slash having you click on the correct bone.
 
 // i need a mouse interaction element
 // have the keyboard do something
 // use state variables
 // use a nested loop
-// resize the window for different device screens
+// take in user input
 
+// let name = prompt("who are you?")
 
 //Stating Image Variables
 let imgSkeleton;
 let imgTitle;
 let imgBack;
 
-// let name = prompt("who are you?")
-
 //Stating Variables
 let clicked = "not";
-let question = "1";
+let question = 1;
+let rect1X;
+let rect1Y;
+let rect1Width;
+let rect1Height;
 
 //Preloading Images
 function preload(){
@@ -42,12 +45,28 @@ function draw(){
   image(imgSkeleton, 300, 100, imgSkeleton.width * 1.5, imgSkeleton.height * 1.5);
   image(imgTitle, 200, 60, imgTitle.width * 0.3, imgTitle.height * 0.3);
 
-  rect(200, 200, 100, 100);
+  //click boxes
+  rect1 = rect(200, 200, 20, 20); //change these from magic numbers
+  rect2 = rect(200, 300, 20, 20);
+  rect3 = rect(200, 350, 20, 20);
+  rect(300, 200, 20, 20);
 
   //Displaying Questions
-  questionOne();
-  displayCorrect();
-  
+  if(question === 1){
+    questionOne();
+    displayCorrect();
+  }
+  else if(question === 2){
+    questionTwo();
+    displayCorrect();
+  }
+  else if(question === 3){
+    questionThree();
+    displayCorrect();
+  }
+  else{
+    text("Congratulations you finished!");
+  }
 }
 
 //Functions
@@ -55,21 +74,40 @@ function questionOne(){
   text("Click on the corrisponding box to the femur bone", 130, 150, 150);
 }
 
-function isInRect(x, y, top, bottom, left, right){
+function isInRect1(x, y, top, bottom, left, right){
+  return x >= left && x <= right && y >= top && y <= bottom;
+}
+
+function isInRect2(x, y, top, bottom, left, right){
+  return x >= left && x <= right && y >= top && y <= bottom;
+}
+
+function isInRect3(x, y, top, bottom, left, right){
   return x >= left && x <= right && y >= top && y <= bottom;
 }
 
 function mouseClicked(){
-  if (isInRect() === true){
+  if (isInRect1() === true && question === 1){
     clicked = "is";
+  }
+  else if(isInRect2() === true && question === 2){
+    clicked = "is";
+  }
+  else if(isInRect3() === true && question === 3){
+    clicked = "is";
+  }
+  else{
+    clicked = "not";
   }
 }
 
 function displayCorrect(){
   if(clicked === "is"){
     text("Correct!", 330, 150, 150);
+    question = question + 1;
   }
   else{
     text("Incorrect", 330, 150, 150);
+    question = question + 1;
   }
 }

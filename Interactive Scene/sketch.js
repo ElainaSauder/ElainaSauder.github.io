eafc5ea21ede73c3a72b3e3d89aa84026abc6762
ElainaSauder <161645329+ElainaSauder@users.noreply.github.com>
2024-03-07 14:04:27 -0600

    fixed code

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -13,8 +13,6 @@
 // use state variables - question??
 // take in user input
 
-// let name = prompt("who are you?")
-
 //Stating Image Variables
 let imgSkeleton;
 let imgTitle;
@@ -23,16 +21,23 @@ let imgBack;
 //Stating Variables
 let clicked = "not"; 
 let question = 1; // My state variable
+let playerName;
+
 let rect1;
 let rect2;
 let rect3;
-let x1 = 330;
-let y1 = 95;
-let x2;
-let y2;
-let x3;
-let y3;
-let squareLength = 5;
+
+let bottom;
+let right;
+let left;
+
+let x1 = 320;
+let y1 = 290;
+let x2 = 340;
+let y2 = 110;
+let x3 = 280;
+let y3 = 280;
+let squareLength = 8;
 
 //Preloading Images
 function preload(){
@@ -43,6 +48,7 @@ function preload(){
 
 function setup() {
   createCanvas(664, 500);
+  playerName = prompt("Welcome! What is your name?"); //extra for experts
 }
 
 function draw(){
@@ -51,13 +57,16 @@ function draw(){
   image(imgTitle, 200, 60, imgTitle.width * 0.3, imgTitle.height * 0.3);
   image(imgSkeleton, 180, 100, imgSkeleton.width * 0.17, imgSkeleton.height * 0.17);
 
-  //click boxes
+  //click boxes and squares
   rect1 = rect(x1, y1, squareLength, squareLength); //change these from magic numbers to the lower mentioned variables
   rect2 = rect(x2, y2, squareLength, squareLength);
   rect3 = rect(x3, y3, squareLength, squareLength);
-  rect(300, 200, squareLength, squareLength);
+  rect(375, 200, squareLength, squareLength);
+  rect(360, 350, squareLength, squareLength);
+  rect(340, 220, squareLength, squareLength);
 
   //Displaying Questions
+  question = 1;
   if(question === 1){
     questionOne();
     displayCorrect();
@@ -71,28 +80,29 @@ function draw(){
     displayCorrect();
   }
   else{
-    text("Congratulations you finished!", 0, 0);
+    text("Congratulations ", playerName, " you finished!", 0, 0); //Extra for experts
   }
 }
 
 //Functions
 function questionOne(){
   fill("white");
-  text("Click on the corresponding box to the femur bone", 130, 150, 150);
+  text("Click on the corresponding box to the femur bone?", 130, 150, 150);
 }
 
 function questionTwo(){
   fill("white");
-  text("Click on the corresponding box to the skull bone", 130, 150, 150);
+  text("Click on the corresponding box to the skull bone?", 130, 150, 150);
 }
 
 function questionThree(){
   fill("white");
-  text("Click on the corresponding box to the metacarpals", 130, 150, 150);
+  text("Click on the corresponding box to the metacarpals?", 130, 150, 150);
 }
 
-function isInRect1(x1, y1, top1, bottom1, left1, right1){
-  return x1 >= left1 && x1 <= right1 && y1 >= top1 && y1 <= bottom1; // make vairables and connect to rects
+function isInRect1(x1, y1, top, bottom, left, right){
+  
+  // return x1 >= left && x1 <= right && y1 >= top && y1 <= bottom; // make vairables and connect to rects
 }
 
 function isInRect2(x, y, top, bottom, left, right){
@@ -117,8 +127,6 @@ function mouseClicked(){
     clicked = "not";
   }
 }
-
-
 
 function displayCorrect(){
   if(clicked === "is"){

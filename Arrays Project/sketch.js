d5e91fce73aa8bc95b80c0a8799e5df6f3094c14
ElainaSauder <161645329+ElainaSauder@users.noreply.github.com>
2024-03-19 14:09:18 -0600

    added arrays project

--- Arrays Project/sketch.js
+++ Arrays Project/sketch.js
@@ -1,10 +1,19 @@
-// Project Title
-// Your Name
-// Date
+// Arrays and Object Notation Project - Basketball shooter
+// Elaina Sauder
+// April 8th, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// use noise to move the nets in a random way
+// use object notations to make the ball and the nets
+// use arrays
+// use an aiming line
+
+//Variables
+let x;
+let y;
+let time = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +21,9 @@ function setup() {
 
 function draw() {
   background(220);
+
+  x = noise(time) * width;
+  y = noise(time + 50) * height;
+  circle(x, y, 50);
+  time += 0.01;
 }

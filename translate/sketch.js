bf5aa66ff9cbfb453374c8efbd608600b34debac
ElainaSauder <161645329+ElainaSauder@users.noreply.github.com>
2024-03-25 17:13:40 -0600

    made slider

--- translate/sketch.js
+++ translate/sketch.js
@@ -18,8 +18,15 @@ function makeAimer(){
   let aimY = 0;
   push(); // save state of the transormation matrix
   translate(width/2, height/2); // moves the origin
-  rotate(mouseX, mouseY);
+  rotate(mouseX);
   line(aimX, aimY, aimX + 70, aimY + 70);
   // strokeWeight();
   pop();
+}
+
+function degreeBar(){
+  fill("green")
+  rect(width/3, height - 20, 360, 10);
+  circle(mouseX, height - 20, 20);
+  Text()
 }
